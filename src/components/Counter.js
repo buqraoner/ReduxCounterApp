58443ca0fea7d84cd762b4ad58f3a9f5fe0023a1
Buğra Öner <boletroy@gmail.com>
2022-03-23 00:18:53 +0300

    vercel

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import { useSelector, useDispatch } from "react-redux";
 import { increment, decrement, reset, incrementByAmount,multiplyByAmount} from "../redux/counter/counterSlice"
 
